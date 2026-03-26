ee54513627562e3df8457f98e262e5dd43f506f5
ruslan39kld <B.ruslan39kld@gmail.com>
2026-03-26 11:40:46 +0300

    fix PORT type

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 
@@ -135,6 +135,7 @@ app.get('*', (_req, res) => {
   res.sendFile(path.join(process.cwd(), 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 3001;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`GigaChat proxy server запущен на http://localhost:${PORT}`);
 });
