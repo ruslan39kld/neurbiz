c00949094f2b394c354cb14eea1bbc14961fb37c
ruslan39kld <B.ruslan39kld@gmail.com>
2026-04-15 10:23:47 +0300

    Update vite config with publicDir

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     publicDir: 'public',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
